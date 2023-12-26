feb69a9638febe2a284a83dd47ce98e688a70b62
EBenze <52275898+ebenze31@users.noreply.github.com>
2023-12-26 12:06:35 +0700

    Update Profile.blade.php

--- resources/views/ProfileUser/Profile.blade.php
+++ resources/views/ProfileUser/Profile.blade.php
@@ -1,1 +1,9 @@
 {{$data}}
+
+<a class="btn btn-sm btn-danger" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();" style="position: absolute;top: 2%;right: 1%;">
+    <i class='bx bx-log-out-circle'></i>
+</a>
+
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+    @csrf
+</form>
