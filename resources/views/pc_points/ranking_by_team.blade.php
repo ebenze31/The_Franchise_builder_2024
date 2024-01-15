cf248b4f2bec553adbb09b0101b41dc135e3e0cc
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-01-15 22:32:32 +0700

    Update ranking_by_team.blade.php

--- resources/views/pc_points/ranking_by_team.blade.php
+++ resources/views/pc_points/ranking_by_team.blade.php
@@ -101,7 +101,7 @@
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
 
-    @media (max-width: 465px) {
+    @media screen and (max-device-width: 465px){
         .other-team {
             padding: 15px 10px 15px 18px;
         }
@@ -112,7 +112,8 @@
                 width: 20%;
             }
     }
-    @media (min-width: 465px) {
+    @media screen and (min-device-width: 465px){
+
         .other-team {
             padding: 15px 10px 15px 25px;
         }
