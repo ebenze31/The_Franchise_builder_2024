7714146dd814d854e81f0139c00f67c52cff8eb4
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-01-17 22:18:39 +0700

    Update export_host.blade.php

--- resources/views/export_host.blade.php
+++ resources/views/export_host.blade.php
@@ -4,6 +4,17 @@
 
 <div class="card">
 	<div class="card-body">
+
+		<div class="row">
+			<div class="col-12">
+				<button id="btn_export_excel" class="btn float-end btn-dark mx-3 d-none" onclick="createExcel()">
+					Export Excel
+				</button>
+			</div>
+		</div>
+
+		<hr>
+		
 		<div class="table-responsive">
 	        <table class="table mb-0 align-middle" id="content_table">
 	            <thead>
@@ -57,6 +68,14 @@ function get_data_host(){
                                 <span class="d-none">{{ url('/img/icon/profile.png') }}</span>`;
                 }
 
+                let html_group_status ;
+                if(result[i].group_status == "Team Ready" || result[i].group_status == "ยืนยันการสร้างบ้านแล้ว"){
+                	html_group_status = `ทีมครบแล้ว`;
+                }
+                else if(result[i].group_status == "มีบ้านแล้ว"){
+                	html_group_status = `กำลังรอสมาชิก`;
+                }
+
 	        	let html = `
 	                <tr class="">
 	                    <td>
@@ -83,7 +102,7 @@ function get_data_host(){
 	                        `+result[i].group_id+`
 	                    </td>
 	                    <td class="text-center">
-	                        `+result[i].group_status+`
+	                        `+html_group_status+`
 	                    </td>
 	                </tr>
 	            `;
@@ -91,11 +110,35 @@ function get_data_host(){
 	            content_tbody.insertAdjacentHTML('beforeend', html); // แทรกล่างสุด
 	        }
 
+            document.querySelector('#btn_export_excel').classList.remove('d-none');
+
         }
         
     });
 
 }
+
+</script>
+
+
+<!-- เพิ่ม jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script src="https://cdn.bootcss.com/html2pdf.js/0.9.1/html2pdf.bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.1/html2pdf.bundle.min.js"></script>
+<script src='https://cdn.jsdelivr.net/npm/table2excel@1.0.4/dist/table2excel.min.js'></script>
+
+<script>
+	
+function createExcel() {
+    let table2excel = new Table2Excel();
+    let currentDate = new Date();
+    let formattedDate = currentDate.toISOString().replace(/[:.]/g, "_"); // สร้างรูปแบบของวันที่ในรูปแบบที่ไม่มีเครื่องหมาย : และ .
+
+    // ตั้งชื่อไฟล์เป็น "รายชื่อสมาชิกทั้งหมด-2023-12-31T12_30_45.678Z.xlsx" (ตัวอย่าง)
+    let fileName = `รายชื่อ Host-${formattedDate}.xlsx`;
+
+    table2excel.export(document.querySelector("#content_table"), fileName);
+};
 
 </script>
 
