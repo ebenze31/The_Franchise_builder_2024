33dd0929e262a9084441f1b2ada0909a0c865bb9
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-01-31 00:52:43 +0700

    Update GroupsController.php

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -661,6 +661,11 @@ class GroupsController extends Controller
             ->orderBy('pc_points.pc_point', 'ASC')
             ->get();
 
+        echo "<pre>";
+        print_r($data_groups);
+        echo "<pre>";
+        exit();
+
         return view('groups.group_show_score' , compact('data_groups'));
 
     }
