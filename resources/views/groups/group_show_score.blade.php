4283d0ef68d75a7c9df28fe6b8c4ff1447a7b3a2
lucky <54930672+luxlnw171041@users.noreply.github.com>
2024-02-01 20:38:02 +0700

    Update group_show_score.blade.php

--- resources/views/groups/group_show_score.blade.php
+++ resources/views/groups/group_show_score.blade.php
@@ -161,7 +161,7 @@ height: 87px;
             <div>
                 <p style="color: #fff;font-size: 14px;font-style: normal;font-weight: 500;line-height: normal;float:right;width: 100%;">Minimum PC 50K</p>
                 <div>
-                    <div class="float-end">
+                    <div class="float-end" style="margin-top:-1px">
                         <i class="fa-regular fa-user text-white me-2"></i>
                         <span style="color: #FCBF29;font-size: 12px;font-style: normal;font-weight: 700;line-height: normal;"><span id="span_amount_member_50k"></span>/10</span>
                     </div>
