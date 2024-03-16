7c9992d24128dbfd5f7d04f8d2bb6692200343e3
EBenze <52275898+ebenze31@users.noreply.github.com>
2024-03-16 19:50:16 +0700

    Update log_web.blade.php

--- resources/views/log_web.blade.php
+++ resources/views/log_web.blade.php
@@ -141,18 +141,18 @@
 
                 for (let i = 0; i < result.length; i++) {
 
-                    let createdAtDate = new Date(result[i].log_create);
+                    // let createdAtDate = new Date(result[i].log_create);
 
-                    let log_create = createdAtDate.toLocaleDateString('th-TH', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric',
-                    });
+                    // let log_create = createdAtDate.toLocaleDateString('th-TH', {
+                    //     year: 'numeric',
+                    //     month: 'long',
+                    //     day: 'numeric',
+                    // });
                     // console.log(result[i].id);
                     data.push([
                         result[i].account ? result[i].account : "--",
                         result[i].name ? result[i].name : "--",
-                        log_create,
+                        result[i].log_create ? result[i].log_create : "--",
                         result[i].log_content ? result[i].log_content : "--",
                         result[i].role ? result[i].role : "--",
                     ]);
